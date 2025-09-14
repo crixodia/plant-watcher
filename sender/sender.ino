74f99cc5db02f0a5e6b98145d4464b7ad0a9bf5c
Cristian Bastidas <crixodia@outlook.com>
2025-09-13 21:54:01 -0500

    :fire: refactor sender

--- sender/sender.ino
+++ sender/sender.ino
@@ -1,6 +1,8 @@
 #include <DHT.h>
 #include <SoftwareSerial.h>
 
+#define BAUD_RATE 9600
+
 DHT dht(12, DHT11);
 SoftwareSerial espSerial(2, 3); // RX, TX
 
@@ -9,30 +11,31 @@ float humidity;     // Humidity
 float soil;         // Soil humidity
 
 void setup() {
-  Serial.begin(9600);
-  espSerial.begin(9600);
+  Serial.begin(BAUD_RATE);
+  espSerial.begin(BAUD_RATE);
   dht.begin();
 }
 
-void loop() {
+void loop() {  
+  if (!espSerial.available()) {
+    Serial.println("ESP-01 is not available.");
+    return 1;
+  }
+
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
   soil = analogRead(A0);
 
-  if (isnan(temperature) || isnan(humidity)) {
-    // Does not send something if sensor fails
-  } else {
+  if (isnan(temperature) || isnan(humidity)){
+    Serial.println("There is a problem reading sensor data.");
+    return 1;
+  }
+
+  String espData = espSerial.readStringUntil('\n');
+  if(espData.equals("READ")){
     String data = String(temperature, 1) + "," + String(humidity, 1) + "," + String(soil);
     espSerial.println(data);
-    // Serial.println(data);
+  } else {
+    Serial.println(espData + " is not a known instruction.");
   }
-
-  if (espSerial.available()) {
-    String espData = espSerial.readStringUntil('\n');
-    if(!espData.equals("")){
-      Serial.println("ESP-01: " + espData);
-    }
-  }
-
-  delay(1000);
 }
