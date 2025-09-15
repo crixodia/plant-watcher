b84365f433c1ae6bcc6b368d99080ecfbda94f07
Cristian Bastidas <crixodia@outlook.com>
2025-09-14 22:26:44 -0500

    :lock: Added basic authentication

--- sender/sender.ino
+++ sender/sender.ino
@@ -1,7 +1,11 @@
+#include <TimeLib.h>
+
 #include <DHT.h>
 #include <SoftwareSerial.h>
 
+
 #define BAUD_RATE 9600
+#define DHT11_FRECUENCY 2000
 
 DHT dht(12, DHT11);
 SoftwareSerial espSerial(2, 3);  // RX, TX
@@ -9,38 +13,82 @@ SoftwareSerial espSerial(2, 3);  // RX, TX
 float temperature;  // Temperatura
 float humidity;     // Humidity
 float soil;         // Soil humidity
+static unsigned long lastRead = 0;
+
+const char* getDate() {
+  static char buffer[15];
+  snprintf(
+    buffer,
+    sizeof(buffer),
+    "%02d%02d%02d%02d%02d%04d",
+    hour(), minute(), second(), day(), month(), year());
+  return buffer;
+}
+
+void printDate() {
+  Serial.print("DATE: ");
+  Serial.println(getDate());
+}
 
 void setup() {
   Serial.begin(BAUD_RATE);
   espSerial.begin(BAUD_RATE);
   dht.begin();
-  delay(5000);
+
+  unsigned long start = millis();
+  while (millis() - start < 5000) {}
+  printDate();
 }
 
 void loop() {
-  if(!espSerial.available()){
-    return -1;
+  if (millis() - lastRead > DHT11_FRECUENCY) {
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+    soil = analogRead(A0);
+    lastRead = millis();
   }
-
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
-  soil = analogRead(A0);
 
   if (isnan(temperature) || isnan(humidity)) {
     Serial.println("There is a problem reading sensor data.");
-    return -1;
+    return;
   }
 
   if (espSerial.available()) {
+
     String espData = espSerial.readStringUntil('\n');
     espData.trim();
     Serial.println("COMMAND: " + espData);
+
     if (espData == "/read") {
-      String data = String(temperature, 1) + "," + String(humidity, 1) + "," + String(soil);
+      char tbuf[6], hbuf[6], data[33];
+
+      dtostrf(temperature, 3, 2, tbuf);
+      dtostrf(humidity, 3, 2, hbuf);
+      snprintf(data, sizeof(data), "%s,%s,%s,%d", getDate(), tbuf, hbuf, (int)soil);
+
       espSerial.println(data);
-      Serial.println("RESPONSE: " + data);
+      Serial.println(data);
+    } else if (espData.startsWith("/set")) {
+      String subcommand = espData.substring(5, 9);
+      String value = espData.substring(10, 26);
+      if (subcommand == "date") {
+        setTime(
+          value.substring(0, 2).toInt(),   // Hour
+          value.substring(2, 4).toInt(),   // Minute
+          value.substring(4, 6).toInt(),   // Second
+          value.substring(6, 8).toInt(),   // Day
+          value.substring(8, 10).toInt(),  // Month
+          value.substring(10, 14).toInt()  // Year
+        );
+        espSerial.println("OK");
+        printDate();
+      } else {
+        Serial.println(subcommand + "is not a valid subcommand for the value " + value + ".");
+      }
+
     } else {
-      Serial.println(espData + " is not a known instruction.");
+
+      Serial.println(espData + " is not a valid instruction.");
     }
   }
 }
--- server/server.ino
+++ server/server.ino
@@ -1,34 +1,47 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ArduinoOTA.h>
 #include <ESP8266mDNS.h>
+#include <uri/UriBraces.h>
 
-#ifndef STASSID
-#define STASSID "SSID"
-#define STAPSK "PASSWORD"
-#define WAIT_FOR_RESPONSE 100
-#endif
+#define STASSID   "SSID"            // Wi-Fi Authentication
+#define STAPSK    "PASSWORD"        // Wi-Fi Authentication
+#define USERNAME  "YOUR_USERNAME"   // Api Authentication
+#define PASSWORD  "YOUR_PASSWORD"   // Api Aithentication
+#define WAIT_FOR_RESPONSE 200
 
 const char *ssid = STASSID;
 const char *password = STAPSK;
+const char *www_username = USERNAME;
+const char *www_password = PASSWORD;
 const char *hostname = "law";
 
 const int port = 80;
 ESP8266WebServer server(port);
 
+void serialFlush() {
+  while (Serial.available() > 0) {
+    Serial.read();
+  }
+}
+
 String read2json(String data) {
   int firstComma = data.indexOf(',');
   int secondComma = data.indexOf(',', firstComma + 1);
-  String t = data.substring(0, firstComma);
-  String h = data.substring(firstComma + 1, secondComma);
-  String s = data.substring(secondComma + 1);
-  return "{\"temperature\":" + t + ",\"humidity\":" + h + ",\"soil\":" + s + "}";
+  int thirdComma = data.indexOf(',', secondComma + 1);
+  String d = data.substring(0, firstComma);
+  String t = data.substring(firstComma + 1, secondComma);
+  String h = data.substring(secondComma + 1, thirdComma);
+  String s = data.substring(thirdComma + 1);
+  return "{\"timestamp\":\"" + d + "\",\"temperature\":" + t + ",\"humidity\":" + h + ",\"soil\":" + s + "}";
 }
 
 String readSerialData(String command) {
+  serialFlush();
   String data = "null,null,null";
   Serial.println(command);
 
-  unsigned long start = millis(); 
+  unsigned long start = millis();
   while (millis() - start < WAIT_FOR_RESPONSE) {
     if (Serial.available()) {
       data = Serial.readStringUntil('\n');
@@ -39,25 +52,61 @@ String readSerialData(String command) {
   return data;
 }
 
+String setSerialData(String command) {
+  serialFlush();
+  String data = "ERROR";
+  Serial.println(command);
+  unsigned long start = millis();
+  while (millis() - start < WAIT_FOR_RESPONSE) {
+    if (Serial.available()) {
+      data = Serial.readStringUntil('\n');
+      data.trim();
+      break;
+    }
+  }
+  return data;
+}
+
+void authenticate(){
+  if (!server.authenticate(www_username, www_password)) { return server.requestAuthentication(); }
+}
+
 void handleStatus() {
+  authenticate();
   server.send(200, "application/json", "{\"status\":\"OK\", \"message\":\"server is running\"}");
 }
 
 void handleRead() {
+  authenticate();
   server.send(200, "application/json", read2json(readSerialData("/read")));
+}
+
+void handleSetDate() {
+  authenticate();
+  if (server.method() != HTTP_PUT) { server.send(405, "application/json", "{\"message\":\"method not allowed.\"}"); }
+  
+  String response = setSerialData("/set date " + server.pathArg(0));
+  if (response == "ERROR") {
+    server.send(501, "application/json", "{\"message\":\"there was an error.\"}");
+    return;
+  }
+
+  server.send(200, "application/json", "{\"message\":\"" + response + "\"}");
 }
 
 void setup(void) {
   Serial.begin(9600);
 
   WiFi.hostname(hostname);
-  WiFi.mode(WIFI_STA); 
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(1000);
+    ESP.restart();
   }
+
+  ArduinoOTA.begin();
 
   Serial.println("");
 
@@ -76,12 +125,14 @@ void setup(void) {
 
   server.on("/status", handleStatus);
   server.on("/read", handleRead);
+  server.on(UriBraces("/set/date/{}"), handleSetDate);
 
   server.begin();
   Serial.println("HTTP listening on port " + String(port));
 }
 
 void loop(void) {
+  ArduinoOTA.handle();
   server.handleClient();
   MDNS.update();
 }
